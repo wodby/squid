703151ce61bd3456679d06069b69264a83957918
Wodby Robot <robot@wodby.com>
2026-09-20 14:18:42 +0000

    Update base image revision to r0

--- base-images.mk
+++ base-images.mk
@@ -4,7 +4,7 @@ BASE_IMAGE_REPOSITORY := wodby/alpine
 BASE_IMAGE_VERSION_SUFFIX :=
 
 BASE_IMAGE_DIGEST_3.24 := sha256:909292f4946048f994caef1e80dffb9508af29bca97dc47f8b738c3e9be30c44
-BASE_IMAGE_DIGEST_3.24-2.20.8 := sha256:4d9d83ce694400ce2113b2511219cb9d0337c991ff4cff5dd35cdf14580cda84
+BASE_IMAGE_DIGEST_3.24-r0 := sha256:a86e97a7768e937427a8768adc5b20935c60fd06a2c994fbccd1e9a5d569b6a6
 
 # Fail before building when a version or variant has no reviewed pin.
 BASE_IMAGE = $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG)@$(or $(BASE_IMAGE_DIGEST_$(BASE_IMAGE_TAG)),$(error No base image digest for $(BASE_IMAGE_REPOSITORY):$(BASE_IMAGE_TAG); update base-images.mk))
